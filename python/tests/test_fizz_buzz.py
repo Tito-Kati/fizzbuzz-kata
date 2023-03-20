d318fe6051368803ac9cf26779d17d9445afde8e
katarn <katarn@katarn.es>
2023-03-20 16:08:34 +0100

    Refactor: FizzBuzz fixture

--- python/tests/test_fizz_buzz.py
+++ python/tests/test_fizz_buzz.py
@@ -1,34 +1,16 @@
+import pytest
 from src.fizz_buzz import FizzBuzz
 
-
 class TestFizzBuzz:
+    @pytest.fixture
+    def fizzBuzz(self):
+        return FizzBuzz()
 
-    def test_returns_string_representation_of_number_1(
-        self,
-    ):
-        # Given
-        fizzBuzz = FizzBuzz()
-        # When
-
-        # Then
+    def test_returns_string_representation_of_number_1(self, fizzBuzz):
         assert fizzBuzz.fizzBuzz(1) == '1'
 
-    def test_returns_string_representation_of_number_2(
-        self,
-    ):
-        # Given
-        fizzBuzz = FizzBuzz()
-        # When
-
-        # Then
+    def test_returns_string_representation_of_number_2(self, fizzBuzz):
         assert fizzBuzz.fizzBuzz(2) == '2'
 
-    def test_returns_fizz_when_3_given(
-        self,
-    ):
-        # Given
-        fizzBuzz = FizzBuzz()
-        # When
-
-        # Then
+    def test_returns_fizz_when_3_given(self, fizzBuzz):
         assert fizzBuzz.fizzBuzz(3) == 'Fizz'
