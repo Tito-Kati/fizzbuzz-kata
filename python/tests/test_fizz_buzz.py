2e3f7499813e516cfff8ed8b13dfff96ab1ea3d6
Luz <luz.maria.agc@gmail.com>
2023-03-20 16:15:04 +0100

    Refactor: better multiple of 3 tests names

--- python/tests/test_fizz_buzz.py
+++ python/tests/test_fizz_buzz.py
@@ -12,8 +12,8 @@ class TestFizzBuzz:
     def test_returns_string_representation_of_number_2(self, fizzBuzz):
         assert fizzBuzz.fizzBuzz(2) == '2'
 
-    def test_returns_fizz_when_3_given(self, fizzBuzz):
+    def test_returns_fizz_when_multiple_of_3_like_3_given(self, fizzBuzz):
         assert fizzBuzz.fizzBuzz(3) == 'Fizz'
 
-    def test_returns_fizz_when_6_given(self, fizzBuzz):
+    def test_returns_fizz_when_multiple_of_3_like_6_given(self, fizzBuzz):
         assert fizzBuzz.fizzBuzz(6) == 'Fizz'
